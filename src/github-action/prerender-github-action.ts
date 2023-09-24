bf916e83f5d7d52bcf3ff01f28bb5a69cdb516e4
Anton Stoychev <antitoxic@gmail.com>
2023-09-24 16:57:49 +0300

    Expose access to more options when used as github action

--- src/github-action/prerender-github-action.ts
+++ src/github-action/prerender-github-action.ts
@@ -13,15 +13,34 @@ void (async () => {
     const maxConcurrentPages = core.getInput('max_concurrent_pages');
     const metaPrerenderOnly = core.getInput('meta_prerender_only');
     const selectorToWaitFor = core.getInput('selector_to_wait_for');
+    const generateSitemapUsingCanonicalBaseUrl =
+      core.getInput('canonical_base_url');
+    const blockedPartialUrls = core
+      .getInput('blocked_partial_urls')
+      .trim()
+      .split('\n')
+      .filter(partialUrl => partialUrl.trim())
+      .filter(Boolean);
 
     httpServer = createStaticFileServer(websiteRoot);
 
     const crawled = await preRenderSite({
-      ...(selectorToWaitFor && { selectorToWaitFor }),
-      metaPrerenderOnly: metaPrerenderOnly === '1',
       startingUrl: 'http://localhost:8080',
-      maxConcurrentPages: Number(maxConcurrentPages),
       outputDir: path.join(process.cwd(), websiteRoot),
+      maxConcurrentPages: Number(maxConcurrentPages),
+      metaPrerenderOnly: metaPrerenderOnly === '1',
+      ...(selectorToWaitFor && { selectorToWaitFor }),
+      ...(generateSitemapUsingCanonicalBaseUrl && {
+        generateSitemapUsingCanonicalBaseUrl,
+      }),
+      ...(blockedPartialUrls.length && {
+        pageOptions: {
+          block: {
+            match: url =>
+              blockedPartialUrls.some(partialUrl => url.includes(partialUrl)),
+          },
+        },
+      }),
       extraBrowserLaunchOptions: {
         /**
          * GitHub actions run as root and chrome won't start unless we disable sandboxing
